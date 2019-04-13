66c575c189556dcf02f269160ee879a97174a7ad
Elzheiz <ezrealgo@gmail.com>
2019-04-14 01:03:55 +0200

    Added ability to pause/unpause the difficulty. Version change but not ready for release yet.
    - Press P to pause/unpause
    - Does not let you change the difficulty while pause, which needs to be corrected.
    - Still let you use the + - buttons, which simply change the offsets "for nothing".

--- DifficultyModifier.cs
+++ DifficultyModifier.cs
@@ -6,13 +6,26 @@ using MonoMod.Cil;
 
 namespace DifficultyModifier
 {
-    [BepInPlugin("com.elzheiz.difficultymodifier", "DifficultyModifier", "1.0")]
+    [BepInPlugin("com.elzheiz.difficultymodifier", "DifficultyModifier", "1.1.0")]
 
     public class DifficultyModifier : BaseUnityPlugin
     {
+        // Control the difficulty in steps
         private int difficultyIncrementIndex = 0;
         private float[] difficultyIncrements = { 1.0f, 10.0f, 60.0f, 600.0f, 3600.0f };
         private float totalDifficultyIncrement = 0.0f;
+
+        // Pause the difficulty and snapshot the various coefficients
+        private bool pausedDifficulty = false;
+        private float pausedDifficultyCoefficient = 0.0f;
+        private float pausedCompensatedDifficultyCoefficient = 0.0f;
+        private float pausedTargetMonsterLevel = 0.0f;
+
+        // Compensate the various coefficients after unpausing to avoid a jump in difficulty
+        private float offsetDifficultyCoefficentJump = 0.0f;
+        private float offsetCompensatedDifficultyCoefficentJump = 0.0f;
+        private float offsetTargetMonsterLevelJump = 0.0f;
+
 
         public void Awake()
         {
@@ -21,6 +34,7 @@ namespace DifficultyModifier
                 ILCursor c = new ILCursor(il).Goto(0);
                 // Get to the next fixedTime load, go to the previous instruction (which should be "this")
                 // Then add the new instruction and remove "this.fixedTime"
+                // DANGEROUS if we leave fixedTime behind -> Infinite loop
                 while (c.Goto(0).TryGotoNext(x => x.MatchLdfld<Run>("fixedTime")))
                 {
                     c.GotoPrev();
@@ -32,6 +46,57 @@ namespace DifficultyModifier
                     });
                     c.RemoveRange(2);
                 }
+
+                // Pause the compensatedDifficultyCoefficient or add the jump offset to it
+                c.Goto(0).TryGotoNext(MoveType.After, x => x.MatchStfld<Run>("compensatedDifficultyCoefficient"));
+                c.EmitDelegate<Action>(() =>
+                {
+                    if (!Run.instance) { return; }
+                    if (pausedDifficulty)
+                    {
+                        // Since we replace the coefficient after it's been calculated,
+                        // Run.instance.compensatedDifficultyCoefficient contains the actual value the coefficient should be, which we can then compare to the paused one
+                        offsetCompensatedDifficultyCoefficentJump = Run.instance.compensatedDifficultyCoefficient - pausedCompensatedDifficultyCoefficient;
+                        Run.instance.compensatedDifficultyCoefficient = pausedCompensatedDifficultyCoefficient;
+                    }
+                    else
+                    {
+                        // If unpaused, add the jump offset to the coefficient to avoid a difficulty jump.
+                        Run.instance.compensatedDifficultyCoefficient -= offsetCompensatedDifficultyCoefficentJump;
+                    }
+                });
+
+                // Pause the difficultyCoefficient or add the jump offset to it
+                c.Goto(0).TryGotoNext(MoveType.After, x => x.MatchStfld<Run>("difficultyCoefficient"));
+                c.EmitDelegate<Action>(() =>
+                {
+                    if (!Run.instance) { return; }
+                    if (pausedDifficulty)
+                    {
+                        offsetDifficultyCoefficentJump = Run.instance.difficultyCoefficient - pausedDifficultyCoefficient;
+                        Run.instance.difficultyCoefficient = pausedDifficultyCoefficient;
+                    }
+                    else
+                    {
+                        Run.instance.difficultyCoefficient -= offsetDifficultyCoefficentJump;
+                    }
+                });
+                
+                // Now pause the targetMonsterLevel or add the jump offset to it
+                c.Goto(0).TryGotoNext(MoveType.After, x => x.MatchCallvirt<Run>("set_targetMonsterLevel"));
+                c.EmitDelegate<Action>(() =>
+                {
+                    if (!Run.instance) { return; }
+                    if (pausedDifficulty)
+                    {
+                        offsetTargetMonsterLevelJump = Run.instance.targetMonsterLevel - pausedTargetMonsterLevel;
+                        typeof(Run).GetProperty("targetMonsterLevel").SetValue(Run.instance, pausedTargetMonsterLevel);
+                    }
+                    else
+                    {
+                        typeof(Run).GetProperty("targetMonsterLevel").SetValue(Run.instance, Run.instance.targetMonsterLevel - offsetTargetMonsterLevelJump);
+                    }
+                });
             };
 
             // Reset the increment when the run is terminated.
@@ -50,13 +115,35 @@ namespace DifficultyModifier
             // Otherwise control time using the + - * / buttons
             if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
             {
-                // I -> Inspect the current difficuly increment and coefficients.
+                // I -> Inspect the current difficulty increment and coefficients.
                 if (Input.GetKeyDown(KeyCode.I))
                 {
                     Debug.Log("compensatedDifficultyCoefficient = " + Run.instance.compensatedDifficultyCoefficient + "\n" +
                         "difficultyCoefficient = " + Run.instance.difficultyCoefficient + "\n" +
                         "targetMonsterLevel = " + Run.instance.targetMonsterLevel + "\n" +
-                        "Total Difficulty Increment = " + totalDifficultyIncrement);
+                        "offsetDifficultyJump = " + offsetDifficultyCoefficentJump + "\n" +
+                        "offsetCompensatedDifficultyCoefficentJump = " + offsetCompensatedDifficultyCoefficentJump + "\n" +
+                        "offsetTargetMonsterLevelJump = " + offsetTargetMonsterLevelJump + "\n" +
+                        "Total Difficulty Increment = " + totalDifficultyIncrement + "\n"
+                    );
+                }
+                // P -> Pause/Unpause the difficulty
+                else if (Input.GetKeyDown(KeyCode.P))
+                {
+                    pausedDifficulty = !pausedDifficulty;
+
+                    if (pausedDifficulty)
+                    {
+                        // Save the values when it's been paused
+                        pausedDifficultyCoefficient = Run.instance.difficultyCoefficient;
+                        pausedCompensatedDifficultyCoefficient = Run.instance.compensatedDifficultyCoefficient;
+                        pausedTargetMonsterLevel = Run.instance.targetMonsterLevel;
+                        Debug.Log("Difficulty paused.");
+                    }
+                    else
+                    {
+                        Debug.Log("Difficulty unpaused.");
+                    }
                 }
                 // + -> Increments the timer
                 else if (Input.GetKeyDown(KeyCode.KeypadPlus))
--- Metadata/README.md
+++ Metadata/README.md
@@ -1,29 +1,31 @@
 # DifficultyModifier
 This mod allows you to control the difficulty of the game during a run.
-It should slide the difficulty bar to show you how difficult the game is.
+You can also decide to pause the difficulty momentarily if it becomes too challenging, and then unpause when you've caught up!
 
 ### Controls
   - CTRL + "Numpad +" -> Increase the difficulty
   - CTRL + "Numpad -" -> Decrease the difficulty
   - CTRL + "Numpad *" -> Increases the difficulty step
   - CTRL + "Numpad /" -> Decreases the difficulty step
-  - CTRL + "Numpad I" -> Check the current difficulty settings (coefficients, monster level and total added difficulty).
+  - CTRL + "P" -> Pause the difficulty
+  - CTRL + "I" -> Check the current debug difficulty settings (coefficients, monster level and total added difficulty).
 
-All the text is displayed in the debug console. Press CTRL + ALT + ` to open it.
+All the debug text is displayed in the Unity console. Press CTRL + ALT + ` to open it.
 
 ### Compatibility
-This mod should be compatible with any other mods available. If you find one it is not compatible with, please open an issue on [GitHub/NewIssue](https://github.com/Elzheiz/ror2-difficulty-modifier/issues/new)
+This mod should be compatible with any other mods available. If you find one it is not compatible with, please open an issue on [GitHub/NewIssue](https://github.com/Elzheiz/ror2-difficulty-modifier/issues/new) or message me on Discord.
 
-This mod has not been tested in Multiplyer, so please be aware it might not work or lead to bugs.
+This mod has not been tested in Multiplyer yet, so please be aware it might not work or lead to bugs.
 
 ### Planned features
 
-  - Pause the difficulty
   - Speed up/Slow down the difficulty increase
   - Config file to control the keybinds and the default settings.
 
 ### Change Notes
 
+#### v1.1.0
+  - Added the ability to pause and unpause the difficulty at will!
 #### v1.0.1
   - Added some text to README.md
 #### v1.0.0 
